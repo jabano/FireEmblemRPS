558b4ff72a74b46a928147f70f8811d8b5856f49
Jev <jabano@users.noreply.github.com>
2018-02-18 00:06:15 -0500

    v1

---
+++ RandomNumberGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace FireEmblemRPS
+{
+    public class RandomNumberGenerator
+    {
+        private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
+
+        public static int NumberBetween(int minimumValue, int maximumValue)
+        {
+            byte[] randomNumber = new byte[4];
+            _generator.GetBytes(randomNumber);
+
+            int randomValue = BitConverter.ToInt32(randomNumber, 0);
+
+            return randomValue;
+          
+        }
+    }
+}
